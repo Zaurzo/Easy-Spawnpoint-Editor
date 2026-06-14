35e72b17f2a92dcea0095d66a00df95ecbc03489
Zaurzo <wafflesfordaysyt@gmail.com>
2026-06-14 17:39:38 -0400

    unselect spawnpoint when tool is holstered

--- lua/weapons/gmod_tool/stools/spawnpoint.lua
+++ lua/weapons/gmod_tool/stools/spawnpoint.lua
@@ -34,6 +34,18 @@ if SERVER then
             self:SetOperation(self:GetClientNumber('index') != 0 and 1 or 0)
         end
     end
+
+    function TOOL:Holster()
+        local wep = self:GetWeapon()
+        local move_ent = wep:GetNWEntity('SpawnpointEditor_MoveEnt')
+        
+        if not IsValid(move_ent) then return end
+
+        move_ent:SetNoCollide(false)
+        move_ent:SetNoDraw(false)
+
+        wep:SetNWEntity('SpawnpointEditor_MoveEnt', NULL)
+    end
 end
 
 function TOOL:SetClientInfo(property, value)
