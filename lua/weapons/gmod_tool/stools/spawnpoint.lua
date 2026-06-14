95c2759cac633b80310f3eb5ef0904d1c8f50f2d
Zaurzo <wafflesfordaysyt@gmail.com>
2026-06-14 17:40:42 -0400

    separate server only tool methods into its own file

--- lua/weapons/gmod_tool/stools/spawnpoint.lua
+++ lua/weapons/gmod_tool/stools/spawnpoint.lua
@@ -24,28 +24,10 @@ TOOL.Information = {
 
 TOOL.SelectDistance = 512
 
-if CLIENT then
-    include('spawnpoint/cl_init.lua')
-end
-
 if SERVER then
-    function TOOL:Think()
-        if self:GetOperation() ~= 2 then
-            self:SetOperation(self:GetClientNumber('index') != 0 and 1 or 0)
-        end
-    end
-
-    function TOOL:Holster()
-        local wep = self:GetWeapon()
-        local move_ent = wep:GetNWEntity('SpawnpointEditor_MoveEnt')
-        
-        if not IsValid(move_ent) then return end
-
-        move_ent:SetNoCollide(false)
-        move_ent:SetNoDraw(false)
-
-        wep:SetNWEntity('SpawnpointEditor_MoveEnt', NULL)
-    end
+    include('spawnpoint/init.lua')
+else
+    include('spawnpoint/cl_init.lua')
 end
 
 function TOOL:SetClientInfo(property, value)
